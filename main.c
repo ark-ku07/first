2e046fe248b3ec0de997ff3740ae07434b234f63
ark-ku07 <kuznetsov2007.kuznetsov@ya.ru>
2025-10-18 15:31:37 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a % b); }
+
+int main() { int n, m;
+
+printf("n m: ");
+scanf("%d %d", &n, &m);
+
+if (n <= 0 || m <= 0) {
+    printf("Ошибка\n");
+    return 0;
+}
+
+int res = (n * m) / gcd(n, m);
+printf("НОК = %d\n", res);
+
+return 0;
+}
