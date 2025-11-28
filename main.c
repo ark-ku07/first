075af60bdfceaaf4dcf44ad0a28d417deada7c16
ark-ku07 <kuznetsov2007.kuznetsov@ya.ru>
2025-11-29 02:46:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a % b); }
-
-int main() { int n, m;
-
-printf("n m: ");
-scanf("%d %d", &n, &m);
-
-if (n <= 0 || m <= 0) {
-    printf("Ошибка\n");
-    return 0;
+void puzir(int* arr, int n) {
+    int flag;
+    for (int x = 0; x < n-1; x++) {
+        flag = 0;
+        for (int y = 0; y < n-x-1; y++) {
+            if (arr[y] > arr[y+1]) {
+                int t = arr[y];
+                arr[y] = arr[y+1];
+                arr[y+1] = t;
+                flag = 1;
+            }
+        }
+        if (flag == 0) break;
+    }
 }
 
-int res = (n * m) / gcd(n, m);
-printf("НОК = %d\n", res);
+void vstavka(int *arr, int size) {
+    int key, j;
+    for (int i = 1; i < size; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && key < arr[j]) {
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = key;
+    }
+    
+}
 
-return 0;
+void slianie(int* arr, int n) {
+    if (n < 2) return;
+    int s = n/2;
+    slianie(arr, s);
+    slianie(arr + s, n - s);
+    
+    int* v = malloc(n * sizeof(int));
+    int l = 0, r = 0, g = 0;
+    
+    while (l < s && r < n - s) {
+        if (arr[l] < arr[s + r]) 
+            v[g++] = arr[l++];
+        else 
+            v[g++] = arr[s + r++];
+    }
+    while (l < s) v[g++] = arr[l++];
+    while (r < n - s) v[g++] = arr[s + r++];
+    
+    for (l = 0; l < n; l++) arr[l] = v[l];
+    free(v);
+}
+
+int main() {
+    srand(time(0));
+    int r[] = {10, 1000, 10000, 100000};
+    
+    printf("Размер массива | Пузырьковая | Вставками | Слиянием\n");
+    printf("---------------------------------------------------\n");
+    
+    for (int z = 0; z < 4; z++) {
+        int size = r[z];
+        int* arr = malloc(size * sizeof(int));
+        
+        for (int w = 0; w < size; w++) 
+            arr[w] = rand() % 100000;
+        
+        double time[3];
+        
+        for (int alg = 0; alg < 3; alg++) {
+            int* copy = malloc(size * sizeof(int));
+            for (int c = 0; c < size; c++) 
+                copy[c] = arr[c];
+            
+            clock_t start = clock();
+            if (alg == 0) puzir(copy, size);
+            else if (alg == 1) vstavka(copy, size);
+            else slianie(copy, size);
+            clock_t end = clock();
+            
+            time[alg] = (double)(end - start) / CLOCKS_PER_SEC;
+            free(copy);
+        }
+        
+        printf("%d | %f | %f | %f\n", size, time[0], time[1], time[2]);
+        free(arr);
+    }
+    
+    return 0;
 }
